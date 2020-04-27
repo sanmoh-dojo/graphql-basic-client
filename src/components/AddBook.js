1954a0da8b6563ac6b38e406294a503528ea8f30
Sanmoh Hombal <devsanmoh@gmail.com>
2020-04-28 02:32:13 +0530

    Everything else before mutation

---
+++ src/components/AddBook.js
@@ -0,0 +1,65 @@
+import React, {Component} from "react";
+import {graphql} from "react-apollo";
+import {getAuthorsQuery} from "../queries/queries";
+
+class AddBook extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      genre: "",
+      authorId: ""
+    };
+  }
+
+  displayAuthors() {
+    var data = this.props.data;
+    if (data.loading) {
+      return <option disabled>Loading</option>;
+    } else {
+      return data.authors.map(author => (
+        <option key={author.id} value={author.id}>
+          {author.name}
+        </option>
+      ));
+    }
+  }
+
+  submitForm(event) {
+    event.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <form id="add-book" onSubmit={this.submitForm.bind(this)}>
+        <div className="field">
+          <label>Book Name : </label>
+          <input
+            type="text"
+            onChange={event => this.setState({name: event.target.value})}
+          />
+        </div>
+        <div className="field">
+          <label>Genre:</label>
+          <input
+            type="text"
+            onChange={event => this.setState({genre: event.target.value})}
+          />
+        </div>
+        <div className="field">
+          <label>Author:</label>
+          <select
+            onChange={event => this.setState({authorId: event.target.value})}
+          >
+            <option>Select author</option>
+            {this.displayAuthors()}
+          </select>
+        </div>
+        <button>+</button>
+      </form>
+    );
+  }
+}
+
+export default graphql(getAuthorsQuery)(AddBook);
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,15 +1,26 @@
 import React, {Component} from "react";
+import {graphql} from "react-apollo";
+import {getBooksQuery} from "../queries/queries";
 
 class BookList extends Component {
+  displayBooks() {
+    var data = this.props.data;
+    if (data.loading) {
+      return <div>Loading books...</div>;
+    } else {
+      return data.books.map(book => {
+        return <li key={book.id}>{book.name}</li>;
+      });
+    }
+  }
   render() {
+    console.log(this.props);
     return (
       <div>
-        <ul id="book-list">
-          <li>Book name</li>
-        </ul>
+        <ul id="book-list">{this.displayBooks()}</ul>
       </div>
     );
   }
 }
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);
